8cc85722cef4a528fd054a8cb8a078488e90dfc5
Redhotzebracakes <goblealex0@gmail.com>
2025-02-05 12:31:44 -0500

    Added some movement

---
+++ Training Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigid_body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector2(3, -2);
+        _rigid_body = GetComponent<Rigidbody2D>();
+        _rigid_body.velocity = Vector2.right * 5f;
+        Invoke(nameof(AcceptDefeat), 12);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void AcceptDefeat()
+    {
+        Destroy(gameObject);
+    }
+
+    
+}
