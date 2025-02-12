cfa981a5a1da0cddd14493fd291afc33776cd027
Redhotzebracakes <goblealex0@gmail.com>
2025-02-12 12:26:13 -0500

    Better Projectiles

--- Training Project/Assets/Scripts/PlayerController.cs
+++ Training Project/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
     private PlayerInput _input; //field to reference Player Input component
     private Rigidbody2D _rigidbody;
     public GameObject ballPrefab;
-
+    private Vector2 _facingVector = Vector2.right;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,7 +33,7 @@ public class PlayerController : MonoBehaviour
         {
             //Debug.Log("Fire activated!");
             var ball = Instantiate(ballPrefab, transform.position, Quaternion.identity);
-            ball.GetComponent<Rigidbody2D>().velocity = Vector2.left * 10f;
+            ball.GetComponent<Rigidbody2D>().velocity =_facingVector.normalized * 10f;
         }
     }
 
@@ -44,5 +44,9 @@ public class PlayerController : MonoBehaviour
 
         //change the velocity to match the Move (every physics update)
         _rigidbody.velocity = dir * 5;
+        if (dir.magnitude > 0.5)
+        {
+            _facingVector = _rigidbody.velocity;
+        }
     }
 }
